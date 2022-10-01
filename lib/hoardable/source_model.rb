cef85d1b870a1811f1e893a0115bfe6b8dd032f1
justin talbott <justin@waymondo.com>
2022-10-01 00:27:03 -0400

    move assignment of _during timespan to version initialization

--- lib/hoardable/source_model.rb
+++ lib/hoardable/source_model.rb
@@ -123,7 +123,8 @@ module Hoardable
             {
               _event_uuid: find_or_initialize_hoardable_event_uuid,
               _operation: operation,
-              _data: initialize_hoardable_data.merge(changes: source_model.changes)
+              _data: initialize_hoardable_data.merge(changes: source_model.changes),
+              _during: ((previous_temporal_tsrange_end || hoardable_source_epoch)..Time.now.utc)
             }
           )
         )
@@ -146,6 +147,32 @@ module Hoardable
         return if source_model.class.connection.transaction_open?
 
         Thread.current[:hoardable_event_uuid] = nil
+      end
+
+      def previous_temporal_tsrange_end
+        source_model.versions.only_most_recent.pluck('_during').first&.end
+      end
+
+      def hoardable_source_epoch
+        if source_model.class.column_names.include?('created_at')
+          source_model.created_at
+        else
+          maybe_warn_about_missing_created_at_column
+          Time.at(0).utc
+        end
+      end
+
+      def maybe_warn_about_missing_created_at_column
+        return unless source_model.class.hoardable_config[:warn_on_missing_created_at_column]
+
+        source_table_name = source_model.class.table_name
+        Hoardable.logger.info(
+          <<~LOG
+            '#{source_table_name}' does not have a 'created_at' column, so the first version’s temporal period
+            will begin at the unix epoch instead. Add a 'created_at' column to '#{source_table_name}'
+            or set 'Hoardable.warn_on_missing_created_at_column = false' to disable this message.
+          LOG
+        )
       end
     end
     private_constant :Service
--- lib/hoardable/version_model.rb
+++ lib/hoardable/version_model.rb
@@ -21,8 +21,6 @@ module Hoardable
       alias_attribute :hoardable_event_uuid, :_event_uuid
       alias_attribute :hoardable_during, :_during
 
-      before_create { hoardable_version_service.assign_temporal_tsrange }
-
       # @!scope class
       # @!method trashed
       # @return [ActiveRecord<Object>]
@@ -137,36 +135,6 @@ module Hoardable
           .attributes_before_type_cast
           .without('hoardable_source_id')
           .reject { |k, _v| k.start_with?('_') }
-      end
-
-      def previous_temporal_tsrange_end
-        hoardable_source.versions.only_most_recent.pluck('_during').first&.end
-      end
-
-      def hoardable_source_epoch
-        if hoardable_source.class.column_names.include?('created_at')
-          hoardable_source.created_at
-        else
-          maybe_warn_about_missing_created_at_column
-          Time.at(0).utc
-        end
-      end
-
-      def assign_temporal_tsrange
-        version_model._during = ((previous_temporal_tsrange_end || hoardable_source_epoch)..Time.now.utc)
-      end
-
-      def maybe_warn_about_missing_created_at_column
-        return unless hoardable_source.class.hoardable_config[:warn_on_missing_created_at_column]
-
-        source_table_name = hoardable_source.class.table_name
-        Hoardable.logger.info(
-          <<~LOG
-            '#{source_table_name}' does not have a 'created_at' column, so the first version’s temporal period
-            will begin at the unix epoch instead. Add a 'created_at' column to '#{source_table_name}'
-            or set 'Hoardable.warn_on_missing_created_at_column = false' to disable this message.
-          LOG
-        )
       end
     end
     private_constant :Service
