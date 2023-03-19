ab6c2b72f82fc337a141a553e290d4a6ccce5863
justin talbott <justin@waymondo.com>
2023-03-19 12:09:33 -0400

    add description

--- lib/generators/hoardable/migration_generator.rb
+++ lib/generators/hoardable/migration_generator.rb
@@ -9,7 +9,12 @@ module Hoardable
   class MigrationGenerator < ActiveRecord::Generators::Base
     source_root File.expand_path('templates', __dir__)
     include Rails::Generators::Migration
-    class_option :foreign_key_type, type: :string
+    class_option(
+      :foreign_key_type,
+      type: :string,
+      optional: true,
+      desc: 'explictly set / override the foreign key type of the versions table'
+    )
 
     def create_versions_table
       migration_template 'migration.rb.erb', "db/migrate/create_#{singularized_table_name}_versions.rb"
