fc67afe8babc251575ac55323b4c1285b8ebaf03
Andrew Buntine <a.buntine@cultureamp.com>
2025-01-18 23:53:33 +1100

    fix: Use older syntax to make build happy

--- lib/hoardable/arel_crud.rb
+++ lib/hoardable/arel_crud.rb
@@ -1,9 +1,9 @@
 module Hoardable
-  # Monkey-patches an internal Arel method to ensure that bulk UPDATE's always operates
+  # Monkey-patches an internal Arel method to ensure that bulk UPDATE's always operate
   # directly on the root table by injecting the ONLY keyword.
   module ArelCrud
-    def compile_update(*)
-      um = super(*)
+    def compile_update(*args)
+      um = super(*args)
 
       if source.left.instance_variable_get(:@klass).in?(Hoardable::REGISTRY)
         return um.table(Arel.sql("ONLY #{source.left.name}"))
